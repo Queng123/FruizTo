fb49eeb110dd061355e899fb61f7eecb3c02b301
lhay9 <lylian.hay@epitech.eu>
2023-12-01 18:44:41 +0100

    feat(main): add the handle move and collisions basic

--- main.lua
+++ main.lua
@@ -2,50 +2,101 @@ wf = require 'windfield'
 
 fruits = {}
 score = 0
+collisionDetected = false
 
 function love.load()
-    world = wf.newWorld(0, 800, true)
-    world:setGravity(0, 512)
+    -- Création du monde
+    world = wf.newWorld(0, 512, true)
+    world:addCollisionClass('Platform')
+    world:addCollisionClass('Player')
+    
+    -- Création des éléments du jeu
+    createGround()
+    createPlatform()
+    createPlayer()
 
-    box = world:newRectangleCollider(400 - 50/2, 0, 50, 50)
-    box:setRestitution(0.4)
-    box:applyAngularImpulse(5000)
-
-    ground = world:newRectangleCollider(0, 550, 800, 50)
-    wall_left = world:newRectangleCollider(0, 0, 50, 600)
-    wall_right = world:newRectangleCollider(750, 0, 50, 600)
-
-    ground:setType('static')
-    wall_left:setType('static')
-    wall_right:setType('static')
-
-    generateRandomRectangles()
+    -- Définition de la fonction de pré-solve pour la plateforme
+    player:setPreSolve(playerPlatformPreSolve)
 end
 
-function generateRandomRectangles()
-    local x = love.math.random(0, 750)
-    local y = 0
-    local width = love.math.random(20, 50)
-    local height = love.math.random(20, 50)
-
-    local randomRect = world:newRectangleCollider(x, y, width, height)
-    randomRect:setRestitution(0.4)
-
-    table.insert(fruits, randomRect)
-end
-
-function love.update(dt)
-    world:update(dt)
-
-    if love.timer.getTime() % 1 == 0 then
-        generateRandomRectangles()
+function love.keypressed(key)
+    -- Gestion des touches pressées
+    if key == 'space' then
+        player:applyLinearImpulse(0, -1000)
+    elseif key == 'r' then
+        resetPlayerPosition()
+    elseif key == 'escape' then
+        love.event.quit()
+    elseif key == 'q' then
+        player:applyLinearImpulse(-200, 0)
+    elseif key == 'd' then
+        player:applyLinearImpulse(200, 0)
     end
 end
 
+function love.update(dt)
+    -- Mise à jour du monde
+    world:update(dt)
+
+    -- Génération de fruits aléatoires
+    if love.timer.getTime() % 1 == 0 then
+        generateRandomRectangles()
+    end
+
+    -- Gestion des collisions avec les fruits
+    handleFruitCollisions()
+
+    -- Mise à jour du score et destruction des fruits touchant le sol
+    updateScoreAndDestroyFruits()
+end
+
 function love.draw()
+    -- Affichage du monde et du score
     world:draw()
-
-
-
     love.graphics.print("Score: " .. score, 10, 10)
+end
+
+-- Fonctions utilitaires
+
+function createGround()
+    ground = world:newRectangleCollider(100, 500, 600, 50)
+    ground:setType('static')
+end
+
+function createPlatform()
+    platform = world:newRectangleCollider(350, 400, 100, 20)
+    platform:setType('static')
+    platform:setCollisionClass('Platform')
+end
+
+function createPlayer()
+    player = world:newRectangleCollider(390, 450, 20, 40)
+    player:setCollisionClass('Player')
+end
+
+function playerPlatformPreSolve(collider_1, collider_2, contact)
+    if collider_1.collision_class == 'Player' and collider_2.collision_class == 'Platform' then
+        local px, py = collider_1:getPosition()
+        local pw, ph = 20, 40
+        local tx, ty = collider_2:getPosition()
+        local tw, th = 100, 20
+        if py + ph/2 > ty - th/2 then contact:setEnabled(false) end
+    end
+end
+
+function resetPlayerPosition()
+    player:setPosition(390, 450)
+    player:setLinearVelocity(0, 0)
+end
+
+function generateRandomRectangles()
+    -- Implementation de la génération aléatoire des rectangles (à compléter selon votre logique)
+end
+
+function handleFruitCollisions()
+    -- Gestion des collisions avec les fruits (à compléter selon votre logique)
+end
+
+function updateScoreAndDestroyFruits()
+    -- Mise à jour du score et destruction des fruits touchant le sol (à compléter selon votre logique)
 end

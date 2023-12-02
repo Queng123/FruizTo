8bb076b902498a5c5f793459231c2b3b2abd4f8f
Raphaël Brenn <raphael.brenn@epitech.eu>
2023-12-02 15:18:59 +0100

    refactor(assets): moved ball images from balls/ to assets/

--- balls.lua
+++ balls.lua
@@ -1,31 +1,31 @@
 local Forretress = {
     size = 20,
     points = 1,
-    texturePath = "balls/forretress.png"
+    texturePath = "assets/forretress.png"
 }
 
 local Jigglypuff = {
     size = 30,
     points = 2,
-    texturePath = "balls/jigglypuff.png"
+    texturePath = "assets/jigglypuff.png"
 }
 
 local Mareep = {
     size = 40,
     points = 4,
-    texturePath = "balls/mareep.png"
+    texturePath = "assets/mareep.png"
 }
 
 local Solrock = {
     size = 50,
     points = 8,
-    texturePath = "balls/solrock.png"
+    texturePath = "assets/solrock.png"
 }
 
 local Whirlipede = {
     size = 60,
     points = 16,
-    texturePath =  "balls/whirlipede.png"
+    texturePath =  "assets/whirlipede.png"
 }
 
 return {

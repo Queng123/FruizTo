69e366f8a9b5d24f703975416162287ca521efd6
Raphaël Brenn <raphael.brenn@epitech.eu>
2023-12-02 13:52:25 +0100

    feat(balls): added 5 ball types containing size, points and texture

---
+++ balls.lua
@@ -0,0 +1,37 @@
+local Forretress = {
+    size = 20,
+    points = 1,
+    texturePath = "balls/forretress.png"
+}
+
+local Jigglypuff = {
+    size = 30,
+    points = 2,
+    texturePath = "balls/jigglypuff.png"
+}
+
+local Mareep = {
+    size = 40,
+    points = 4,
+    texturePath = "balls/mareep.png"
+}
+
+local Solrock = {
+    size = 50,
+    points = 8,
+    texturePath = "balls/solrock.png"
+}
+
+local Whirlipede = {
+    size = 60,
+    points = 16,
+    texturePath =  "balls/whirlipede.png"
+}
+
+return {
+    forretress = Forretress,
+    jigglypuff = Jigglypuff,
+    mareep = Mareep,
+    solrock = Solrock,
+    whirlipede = Whirlipede
+}
--- main.lua
+++ main.lua
@@ -1,102 +1,60 @@
-wf = require 'windfield'
+ballTypes = require 'balls'
 
-fruits = {}
-score = 0
-collisionDetected = false
+local world = love.physics.newWorld(0, 9.81*64, true)
+local balls = {}
 
-function love.load()
-    -- Création du monde
-    world = wf.newWorld(0, 512, true)
-    world:addCollisionClass('Platform')
-    world:addCollisionClass('Player')
-    
-    -- Création des éléments du jeu
-    createGround()
-    createPlatform()
-    createPlayer()
-
-    -- Définition de la fonction de pré-solve pour la plateforme
-    player:setPreSolve(playerPlatformPreSolve)
+-- Create balls
+function spawnNewBall(ballType, x, y)
+    local ball = {
+        size = ballType.size, 
+        points = ballType.points, 
+        texturePath = ballType.texturePath
+    }
+    ball.body = love.physics.newBody(world, x, y, 'dynamic')
+    ball.shape = love.physics.newCircleShape(ball.size)
+    ball.fixture = love.physics.newFixture(ball.body, ball.shape, 1)
+    ball.fixture:setRestitution(0.4)
+    ball.texture = love.graphics.newImage(ball.texturePath)
+    table.insert(balls, ball)
 end
 
-function love.keypressed(key)
-    -- Gestion des touches pressées
-    if key == 'space' then
-        player:applyLinearImpulse(0, -1000)
-    elseif key == 'r' then
-        resetPlayerPosition()
-    elseif key == 'escape' then
-        love.event.quit()
-    elseif key == 'q' then
-        player:applyLinearImpulse(-200, 0)
-    elseif key == 'd' then
-        player:applyLinearImpulse(200, 0)
+spawnNewBall(ballTypes.forretress, 100, 200)
+spawnNewBall(ballTypes.jigglypuff, 200, 200)
+spawnNewBall(ballTypes.mareep, 320, 200)
+spawnNewBall(ballTypes.solrock, 460, 200)
+spawnNewBall(ballTypes.whirlipede, 620, 200)
+
+-- Create walls and floor
+local walls = {}
+for i=1,3 do
+    walls[i] = {}
+    walls[i].body = love.physics.newBody(world, 0, 0, 'static')
+    if i == 1 then
+        -- floor
+        walls[i].shape = love.physics.newRectangleShape(400, 575, 800, 50)
+    elseif i == 2 then
+        -- left wall
+        walls[i].shape = love.physics.newRectangleShape(25, 300, 50, 600)
+    else
+        -- right wall
+        walls[i].shape = love.physics.newRectangleShape(775, 300, 50, 600)
     end
+    walls[i].fixture = love.physics.newFixture(walls[i].body, walls[i].shape)
 end
 
-function love.update(dt)
-    -- Mise à jour du monde
-    world:update(dt)
-
-    -- Génération de fruits aléatoires
-    if love.timer.getTime() % 1 == 0 then
-        generateRandomRectangles()
-    end
-
-    -- Gestion des collisions avec les fruits
-    handleFruitCollisions()
-
-    -- Mise à jour du score et destruction des fruits touchant le sol
-    updateScoreAndDestroyFruits()
-end
-
+-- Draw the ball and walls
 function love.draw()
-    -- Affichage du monde et du score
-    world:draw()
-    love.graphics.print("Score: " .. score, 10, 10)
-end
-
--- Fonctions utilitaires
-
-function createGround()
-    ground = world:newRectangleCollider(100, 500, 600, 50)
-    ground:setType('static')
-end
-
-function createPlatform()
-    platform = world:newRectangleCollider(350, 400, 100, 20)
-    platform:setType('static')
-    platform:setCollisionClass('Platform')
-end
-
-function createPlayer()
-    player = world:newRectangleCollider(390, 450, 20, 40)
-    player:setCollisionClass('Player')
-end
-
-function playerPlatformPreSolve(collider_1, collider_2, contact)
-    if collider_1.collision_class == 'Player' and collider_2.collision_class == 'Platform' then
-        local px, py = collider_1:getPosition()
-        local pw, ph = 20, 40
-        local tx, ty = collider_2:getPosition()
-        local tw, th = 100, 20
-        if py + ph/2 > ty - th/2 then contact:setEnabled(false) end
+    for i=1,3 do
+        love.graphics.polygon('line', walls[i].body:getWorldPoints(walls[i].shape:getPoints()))
+    end
+    for i, ball in ipairs(balls) do
+        local scaleX = ball.size * 2 / (ball.texture:getWidth() - 30)
+        local scaleY = ball.size * 2 / (ball.texture:getHeight() - 30)
+        love.graphics.draw(ball.texture, ball.body:getX(), ball.body:getY(), 0, scaleX, scaleY, ball.texture:getWidth()/2, ball.texture:getHeight()/2)
     end
 end
 
-function resetPlayerPosition()
-    player:setPosition(390, 450)
-    player:setLinearVelocity(0, 0)
-end
-
-function generateRandomRectangles()
-    -- Implementation de la génération aléatoire des rectangles (à compléter selon votre logique)
-end
-
-function handleFruitCollisions()
-    -- Gestion des collisions avec les fruits (à compléter selon votre logique)
-end
-
-function updateScoreAndDestroyFruits()
-    -- Mise à jour du score et destruction des fruits touchant le sol (à compléter selon votre logique)
+-- Update the physics world
+function love.update(dt)
+    world:update(dt)
 end

3ae21b318990b96a6747f219b76652c0093c7d21
Raphaël Brenn <raphael.brenn@epitech.eu>
2023-12-03 00:00:35 +0100

    feat(collisions): functionning ball container

--- HandleKeyPressed.lua
+++ HandleKeyPressed.lua
@@ -7,7 +7,8 @@ end
 function HandleKeyPressed.handleInputMenu(key)
     if key == 'space' then
         currentState = GameState.GAME
-        spawnNewBall(BallTypes.Forretress, 200, 200)
+        spawnNewBall(BallTypes.Jigglypuff, 200, 200)
+        spawnNewBall(BallTypes.Jigglypuff, 220, 100)
     end
 end
 

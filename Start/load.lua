0427b7c18d43cbf1c84730610ddb285d20033638
unknown <matthias.gaste@epitech.eu>
2023-12-03 13:58:36 +0100

    feat(): add new scene before cinematique

---
+++ Start/load.lua
@@ -0,0 +1,8 @@
+LoadStart = {}
+
+function LoadStart.loadStart()
+    logotype = love.graphics.newImage("assets/logotype.png")
+    logotypeWidth = 800
+    logotypeHeight = 350
+    font = love.graphics.newFont("assets/PixeloidSans-mLxMm.ttf", 40)
+end
